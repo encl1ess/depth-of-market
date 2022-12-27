2b766aa0caa193a587fd0acc459927e13265a780
Eliza Erzanukaeva <akribophobia@outlook.com>
2022-12-27 23:57:22 +0400

    feat: hook and event bus

---
+++ src/hooks/useDepth.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { onMounted, ref } from 'vue';
+
+export function useDepth(symbol='BTCUSDT', limit = 500) {
+    const depthRecords = ref([]);
+    const isDepthLoading = ref(true);
+    const fetching = async () => {
+        try {
+            const response = await axios.get(`https://api.binance.com/api/v3/depth?symbol=${symbol}&limit=${limit}`)
+            depthRecords.value = response.data;
+            console.log(depthRecords)
+        } catch (e) {
+            alert(e)
+        } finally {
+            isDepthLoading.value = false;
+        }
+    }
+
+    onMounted(fetching) //функция должна отрабатываться, когда компонент монтируется
+    return {
+        depthRecords, isDepthLoading
+    }
+
+}
---
+++ src/views/MainView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        SPA app "Depth of Market"
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
