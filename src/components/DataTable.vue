874a9324da55d9ca9eeab0343b2836b3bebf24f8
Eliza Erzanukaeva <akribophobia@outlook.com>
2022-12-30 02:08:21 +0400

    feat(bookView): add hover scroll

---
+++ src/components/DataTable.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-table class="data-table m-0 mb-10" fixed-header theme="light">
+    <!-- <caption class="data-table__caption text-white text-center text-uppercase bg-blue-lighten-2"> {{ title }}</caption> -->
+    <thead>
+      <tr>
+        <th :colspan="headers.length" class=" text-white text-center text-uppercase bg-blue-lighten-2">{{ title }}</th>
+      </tr>
+      <tr>
+        <th class="bg-blue" v-for="header in headers">
+          {{ header.text }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in data">
+        <td class="text-left">{{ item[0] }}</td>
+        <td class="text-left">{{ item[1] }}</td>
+        <td class="text-left">{{ item[0] * item[1] }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+export default {
+  name: 'data-table',
+  props: {
+    title: String,
+    headers: Array,
+    data: Array,
+    colspan: Number
+  }
+}
+</script>
+
+<style lang="scss">
+.data-table {
+  width: 50%;
+  min-width: max-content;
+
+  .v-table__wrapper {
+
+    overflow-y: scroll;
+    height: 80vh;
+
+    &::-webkit-scrollbar {
+      visibility: hidden;
+    }
+
+    &:hover {
+      &::-webkit-scrollbar {
+        visibility: visible;
+        background: lightgrey;
+        border-radius: 2em;
+
+        &-thumb {
+          background: #2196F3;
+          border-radius: 2em;
+        }
+      }
+    }
+  }
+
+
+  @media screen and (max-width: 1290px) {
+    width: 90%;
+
+
+  }
+}
+
+@media(max-width: 550px) {
+
+  th:nth-child(3),
+  td:nth-child(3) {
+    display: none;
+  }
+}
+</style>
--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -1,40 +1,78 @@
 <template>
-    <div class="book">
-        <v-table :headers="headers" :items="records"></v-table>
-    </div>
+    <Loader v-if="isDepthLoading" />
+    <v-container v-else class="book-view d-flex flex-column">
+        <v-select @update:model-value="scrollPositionHandler" class="book-view__drop-down align-self-center mb-1"
+            :items="['asks', 'bids']" label="table"></v-select>
+        <v-container class="book-view__table d-flex flex-wrap align-start justify-center">
+            <data-table :headers="headers" :data="depthRecords.asks" id="asks" title="Asks"></data-table>
+            <data-table :headers="headers" :data="depthRecords.bids" id="bids" title="Bids"></data-table>
+        </v-container>
+
+    </v-container>
 </template>
 
 <script>
 import { useDepth } from '@/hooks/useDepth';
-
+import DataTable from '@/components/DataTable.vue';
+import Loader from '@/ui/Loader.vue';
 export default {
+    components: {
+        DataTable, Loader
+    },
     data() {
         return {
+            scrollPosition: 0,
             headers: [
                 {
                     text: 'Price',
-                    align: 'start',
                     value: 'price',
                 },
                 {
                     text: 'Quantity',
                     value: 'quantity',
-                }
+                }, {
+                    text: 'Amount',
+                    value: 'amount',
+                },
             ]
         }
     },
-    setup(props) {
-        const { records, isDepthLoading } = useDepth('BTCUSDT');
+    setup() {
+        const { depthRecords, isDepthLoading } = useDepth('BTCUSDT');
         return {
-            records,
+            depthRecords,
             isDepthLoading
         }
+    },
+    methods: {
+        scrollPositionHandler(value) {
+            document.getElementById(value).scrollIntoView({
+                behavior: "smooth"
+            });
+        },
+
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.book {
-    overflow-y: auto;
+.book-view {
+    height: 85vh;
+    &__drop-down {
+        display: none;
+
+        @media(max-width: 1290px) {
+            display: flex;
+        }
+    }
+    &__table {
+        position: relative;
+        height: 85vh;
+        overflow-y: scroll;
+
+        &::-webkit-scrollbar {
+            visibility: hidden;
+        }
+    }
 }
 </style>
